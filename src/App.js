c6d95761266e659f2b68f9c68da2f0c1fba8d753
KalVasFlame <unnameddwar@gmail.com>
2021-05-01 00:28:34 +0300

    +

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserCard from './components/UserCard'
+const App = () => (
+    <div>
+    <p>React-HW-1</p>
+    <UserCard />
+    </div>
+  )
+
+
+export default App;
